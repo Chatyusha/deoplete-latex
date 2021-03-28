9962d5dffec399d962acde42ac9a474c5fded66d
Chatyusha <hirounicat76@gmail.com>
2021-03-28 11:24:32 +0900

    update compile command

--- rplugin/python3/deoplet-latexe/__init__.py
+++ rplugin/python3/deoplet-latexe/__init__.py
@@ -5,7 +5,9 @@ import subprocess
 class NvimLaTeXCompile(object):
     def __init__(self, nvim):
         self.nvim = nvim
-
+        self.path=""
+        self.compiler=""
+        self.driver=""
 
     @pynvim.command('TestCommand')
     def testcommand(self):
@@ -13,36 +15,25 @@ class NvimLaTeXCompile(object):
         test=self.nvim.eval("g:loaded_deolatex")
         self.nvim.command("echo \"g:loaded_deolatex=\""+str(test))
 
-    @pynvim.command('TypeSet')
+    @pynvim.autocmd('BufRead,BufNewFile', pattern='*.tex,*.bib', sync=True)
     def latexTypeset(self):
-        path="/usr/local/texlive/2020/bin/x86_64-linux/latex"
-        texCompiler="platex"
-        driver="dvipdfmx"
+        self.path="/usr/local/texlive/2020/bin/x86_64-linux/latex"
+        self.compiler="platex"
+        self.driver="dvipdfmx"
         if self.nvim.eval("exists('g:latex_path')") == 0:
-            self.nvim.vars["latex_path"] = path
+            self.nvim.vars["latex_path"] = self.path
         if self.nvim.eval("exists('g:latex_compiler')") == 0:
-            self.nvim.vars['latex_compiler'] = texCompiler
+            self.nvim.vars['latex_compiler'] = self.compiler
         if self.nvim.eval("exists('g:latex_driver')") == 0:
-            self.nvim.vars['latex_driver'] = driver
+            self.nvim.vars['latex_driver'] = self.driver
 
-        path=self.nvim.eval("g:latex_path")
-        texCompiler=self.nvim.eval("g:latex_compiler")
-        driver=self.nvim.eval("g:latex_driver")
+        self.path=self.nvim.eval("g:latex_path")
+        self.compiler=self.nvim.eval("g:latex_compiler")
+        self.driver=self.nvim.eval("g:latex_driver")
 
     @pynvim.command('Tex2pdf')
     def LatexCompile(self):
-        path="/usr/local/texlive/2020/bin/x86_64-linux/latex"
-        texCompiler="platex"
-        driver="dvipdfmx"
-        if self.nvim.eval("exists('g:latex_path')") == 0:
-            self.nvim.vars["latex_path"] = path
-        if self.nvim.eval("exists('g:latex_compiler')") == 0:
-            self.nvim.vars['latex_compiler'] = texCompiler
-        if self.nvim.eval("exists('g:latex_driver')") == 0:
-            self.nvim.vars['latex_driver'] = driver
-
-        path=self.nvim.eval("g:latex_path")
-        texCompiler=self.nvim.eval("g:latex_compiler")
-        driver=self.nvim.eval("g:latex_driver")
-        #self.nvim.command("echo '" + str(path)+"'")
+        fname=self.nvim.eval("expand('%:p')")
+        self.nvim.command("echo \"" + fname +"\"")
+        #self.nvim.command("echo 'Now Developping ...'")
 
